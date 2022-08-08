336d126f8f3d04adaec35bb99c93681093707c89
ahmed9123 <ahmed912@hotmail.co.uk>
2022-08-08 15:30:46 +0100

    excerise 1

---
+++ excercise/src/excercisePackage/Calcultor.java
@@ -0,0 +1,31 @@
+package excercisePackage;
+
+public class Calcultor {
+	
+		
+		public int addition(int a, int b)
+		{
+			return a + b;
+		}
+		
+		public int substraction(int a, int b)
+		{
+			return a - b;
+		}
+		
+		public int multiplication(int a, int b)
+		{
+			return a*b;
+		}
+		
+		public double devision(double a, double b)
+		{
+			return a / b;
+		}
+		
+		
+		
+		
+		
+	
+}
---
+++ excercise/src/excercisePackage/Main.java
@@ -0,0 +1,14 @@
+package excercisePackage;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Results pupil1 = new Results();
+		 
+		 System.out.println("Pupil got " + pupil1.Marks(60, 90, 100)  + " as their total marks");
+		
+		System.out.println(pupil1.Percentage());
+  
+	}
+
+}
